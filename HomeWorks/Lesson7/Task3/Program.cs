b49967c8fb436226bd93f77dc27061a20deb0b9b
Anton <Zoom8505@gmail.com>
2022-11-13 23:09:16 +0300

    add home work7

---
+++ HomeWorks/Lesson7/Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrix(int lenRow, int lenColumn)
+{
+    int[,] matrix = new int[lenRow, lenColumn];
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = random.Next(-5, 5);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] AverageColumns(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            array[j] += matrix[i, j];
+        }
+        array[j] /= matrix.GetLength(0);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]:f1}\t");
+    }
+    Console.WriteLine();
+}
+
+int[,] myArray = CreateMatrix(3, 4);
+PrintMatrix(myArray);
+
+Console.WriteLine("Среднее арифметическое каждого столбца:");
+PrintArray(AverageColumns(myArray));
